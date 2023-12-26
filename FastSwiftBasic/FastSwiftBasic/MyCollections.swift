a640a1774de037c8937a13e5679d385affaaa609
JoonYoung Yoon <ZE-R0-1@github.com>
2023-12-26 17:31:19 +0900

    컬렉션 - 여러개의 변수를 만드는 방법

--- FastSwiftBasic/FastSwiftBasic/MyCollections.swift
+++ FastSwiftBasic/FastSwiftBasic/MyCollections.swift
@@ -11,13 +11,25 @@ struct MyCollections: View {
     
     var foods: [String] = ["eggs", "bananas", "beans"]
     var jazzs: Set<String> = ["bibijiji", "labdap", "didididi"]
-    var hiphop: Set<String> = ["labjap", "rap", "wow"]
+    var hiphop: Set<String> = ["labdap", "rap", "wow"]
+    var koEngDict: [String:String] = ["사과":"Apple", "바나나":"Banana"]
     
     var body: some View {
         VStack {
-            Text(foods[0])
-            Text(foods[1])
-            Text(hiphop.intersection(jazzs))
+            
+            Text(hiphop.intersection(jazzs).description)
+            
+            Button {
+                var intersectionMusic = hiphop.intersection(jazzs)
+                intersectionMusic.description
+            } label: {
+                Text("hit!")
+            }
+            
+            Text(koEngDict["사과"]!)
+            Text(koEngDict["바나나"]!)
+            
+            Text(foods[2])
         }
     }
 }
