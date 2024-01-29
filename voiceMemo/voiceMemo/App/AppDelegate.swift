029a250cbece26ec79aab991e6a9060cde3b65f9
JoonYoung Yoon <ZE-R0-1@github.com>
2024-01-29 14:19:05 +0900

    24.1.29

---
+++ voiceMemo/voiceMemo/App/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  voiceMemo
+//
+//  Created by KMUSER on 2024/01/29.
+//
+
+import Foundation
+
+class AppDelegate {
+    
+}
---
+++ voiceMemo/voiceMemo/App/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by KMUSER on 2024/01/29.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
