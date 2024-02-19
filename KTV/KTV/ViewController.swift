926b16752e15c7592a88bd1b57c3e5e4f4bfdfb2
JoonYoung Yoon <ZE-R0-1@github.com>
2024-02-19 12:39:29 +0900

    fix tab space

--- KTV/KTV/ViewController.swift
+++ KTV/KTV/ViewController.swift
@@ -8,19 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func buttonDidTap(_ sender: Any) {
-        print("button did tap")
-    }
-    
-    @IBAction func upperButtonDidTap(_ sender: Any) {
-        print("upper button did tap")
-    }
-    
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  @IBAction func buttonDidTap(_ sender: Any) {
+    print("button did tap")
+  }
+  
+  @IBAction func upperButtonDidTap(_ sender: Any) {
+    print("upper button did tap")
+  }
+  
+  
 }
 
