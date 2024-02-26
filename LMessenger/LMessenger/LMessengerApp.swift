69dddc75aad574e640966df4afa41f0aa988897f
JoonYoung Yoon <ZE-R0-1@github.com>
2024-02-26 17:55:07 +0900

    add Assets / Colors

---
+++ LMessenger/LMessenger/LMessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMessengerApp.swift
+//  LMessenger
+//
+//  Created by KMUSER on 2024/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LMessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
