365b0fa3826e08e3c37d64c734f28c1c97026fcf
JoonYoung Yoon <ZE-R0-1@github.com>
2024-03-18 17:12:41 +0900

    NavigationRouter

---
+++ LMessenger/LMessenger/General/View/SearchButton.swift
@@ -0,0 +1,35 @@
+//
+//  SearchButton.swift
+//  LMessenger
+//
+//  Created by KMUSER on 2024/03/18.
+//
+
+import SwiftUI
+
+struct SearchButton: View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(height: 36)
+                .background(Color.greyCool)
+                .cornerRadius(5)
+            
+            HStack {
+                Text("검색")
+                    .font(.system(size: 12))
+                    .foregroundColor(.greyLightVer2)
+                Spacer()
+            }
+            .padding(.leading, 22)
+        }
+        .padding(.horizontal, 30)
+    }
+}
+
+struct SearchButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchButton()
+    }
+}
---
+++ LMessenger/LMessenger/View/ChatList/ChatListViewModel.swift
@@ -0,0 +1,39 @@
+//
+//  ChatListViewModel.swift
+//  LMessenger
+//
+//  Created by KMUSER on 2024/03/18.
+//
+
+import Foundation
+import Combine
+
+class ChatListViewModel: ObservableObject {
+    
+    enum Action {
+        case load
+    }
+    
+    @Published var chatRooms: [ChatRoom] = []
+    
+    let userId: String
+    
+    private var container: DIContainer
+    private var subscriptions = Set<AnyCancellable>()
+    
+    init(container: DIContainer, userId: String) {
+        self.container = container
+        self.userId = userId
+    }
+    
+    func send(action: Action) {
+        switch action {
+        case .load:
+            container.services.chatRoomService.loadChatRooms(myUserId: userId)
+                .sink { completion in
+                } receiveValue: { [weak self] chatRooms in
+                    self?.chatRooms = chatRooms
+                }.store(in: &subscriptions)
+        }
+    }
+}
