c1b0d105961137da4dfa02349adc5108b498b95a
JoonYoung Yoon <110395625+ZE-R0-1@users.noreply.github.com>
2024-06-24 10:49:48 +0900

    Timer

--- voiceMemo/voiceMemo/App/AppDelegate.swift
+++ voiceMemo/voiceMemo/App/AppDelegate.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  var notificationDelegate = NotificationDelegate()
-  
-  func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-  ) -> Bool {
-    UNUserNotificationCenter.current().delegate = notificationDelegate
-    return true
-  }
+    var notificationDelegate = NotificationDelegate()
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = notificationDelegate
+        return true
+    }
 }
--- voiceMemo/voiceMemo/App/voiceMemoApp.swift
+++ voiceMemo/voiceMemo/App/voiceMemoApp.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 @main
 struct voiceMemoApp: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-  
-  var body: some Scene {
-    WindowGroup {
-      OnboardingView()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
     }
-  }
 }
