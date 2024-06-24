28ded59b0eb1c71510427d043f8847e7b8b2f3a5
JoonYoung Yoon <110395625+ZE-R0-1@users.noreply.github.com>
2024-06-24 10:04:33 +0900

    voiceRecoder

--- voiceMemo/voiceMemo/Core/Extension/Double+Extensions.swift
+++ voiceMemo/voiceMemo/Core/Extension/Double+Extensions.swift
@@ -8,11 +8,12 @@
 import Foundation
 
 extension Double {
-  var formattedTimeInterval: String {
-    let totalSeconds = Int(self)
-    let seconds = totalSeconds % 60
-    let minutes = (totalSeconds / 60) % 60
-    
-    return String(format: "%02d:%02d", minutes, seconds)
-  }
+    // 03:05
+    var formattedTimeInterval: String {
+        let totalSeconds = Int(self)
+        let seconds = totalSeconds % 60
+        let minutes = (totalSeconds / 60) % 60
+        
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
 }
